9ce0213a8994947f58935a1dc02739bbf173e4b9
zhongyan19 <64964519+zhongyan19@users.noreply.github.com>
2024-04-28 18:46:57 +0200

    site.config.js

--- site.config.js
+++ site.config.js
@@ -1,25 +1,23 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "mercury-eucalyptus-63e",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
-    instagram: "",
+    role: "mature developer",
+    bio: "I develop everything in nature.",
+    email: "yan.zhong@etu.unige.ch",
+    Twitter: "@GlacialYan",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `Yan Zhong, Nature Explorer`,
+      href: "https://github.com/zhongyan19/yan.zhong",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "Yan Zhong, Nature Explorer",
+    description: "welcome to Yan Zhong's blog!",
   },
 
   // CONFIG configration (required)
@@ -53,7 +51,7 @@ const CONFIG = {
     },
   },
   utterances: {
-    enable: true,
+    enable: false,
     config: {
       repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
       "issue-term": "og:title",
